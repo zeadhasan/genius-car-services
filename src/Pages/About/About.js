cb5b4e35305cca9aad0501ff39d4a709a3577f0a
zeadhasan <zeadhasan1995shuvo@gmail.com>
2022-04-28 01:50:05 +0600

    Pages commit one

---
+++ src/Pages/About/About.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const About = () => {
+    return (
+        <div>
+            <h2>This is our About </h2>
+        </div>
+    );
+};
+
+export default About;
---
+++ src/Pages/Home/Services/Services.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Services = () => {
+    return (
+        <div>
+            <h2> service Items :</h2>
+        </div>
+    );
+};
+
+export default Services;
